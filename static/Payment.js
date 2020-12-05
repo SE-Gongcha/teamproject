5bc599a5448250b54a870389f64e194e09a6b522
Choi Gwangmin <crazy4u5@naver.com>
2020-12-05 18:12:32 +0900

    20201205_1812

--- static/Payment.js
+++ static/Payment.js
@@ -1,6 +1,6 @@
 var localstorage = this.localStorage
 var lType_of_payment = ['공차 쿠폰', '통신사 할인', '기프티콘'];
-var lWhat_to_do = ['전화 번호를 입력해 주시오', '전화 번호를 입력해 주시오', '기프티콘 번호를 입력해 주시오'];
+var lWhat_to_do = ['전화 번호를', '전화 번호를', '기프티콘 번호를'];
 var lPic_src = ['../static/coupon_scan.jpg', '../static/membership_scan.jpg', '../static/gifticon_scan.jpg'];
 
 function payment_type_set(_type) {
@@ -8,6 +8,7 @@ function payment_type_set(_type) {
     localstorage.removeItem('Pic_src');
 
     localstorage.setItem('Payment_type', lType_of_payment[_type]);
+    localstorage.setItem('What_to_do', lWhat_to_do[_type]);
     localstorage.setItem('Pic_src', lPic_src[_type]);
     if(_type == 0 || _type == 1) {
         localstorage.setItem('Is_Discount', '1');
